aa7d20fa0d338d0c560867b306bac7ce5efac38f
修冶 <18374418321@163.com>
2017-06-04 14:30:26 +0800

    start

---
+++ SwiftCpp_begin/SwiftCppKit/SwiftCppKit/SwiftCppKit.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCppKit.h
+//  SwiftCppKit
+//
+//  Created by 李修冶 on 2017/6/4.
+//  Copyright © 2017年 李修冶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCppKit.
+FOUNDATION_EXPORT double SwiftCppKitVersionNumber;
+
+//! Project version string for SwiftCppKit.
+FOUNDATION_EXPORT const unsigned char SwiftCppKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCppKit/PublicHeader.h>
+
+
