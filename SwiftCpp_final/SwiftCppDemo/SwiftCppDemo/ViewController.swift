8ec16ee4853b4a4fd2cd3a94c63e58b9df8d9c8a
修冶 <18374418321@163.com>
2017-06-04 14:31:21 +0800

    start

---
+++ SwiftCpp_final/SwiftCppDemo/SwiftCppDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SwiftCppDemo
+//
+//  Created by 李修冶 on 2017/6/4.
+//  Copyright © 2017年 李修冶. All rights reserved.
+//
+
+import UIKit
+import SwiftCppKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
