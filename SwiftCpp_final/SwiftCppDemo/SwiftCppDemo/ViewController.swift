ab67b710fef1952e3d7a1512b4b30ee6f087edf9
修冶 <lixiuye@lixiuyedemacbook-air.local>
2017-06-07 22:50:12 +0800

    update

--- SwiftCpp_final/SwiftCppDemo/SwiftCppDemo/ViewController.swift
+++ SwiftCpp_final/SwiftCppDemo/SwiftCppDemo/ViewController.swift
@@ -22,7 +22,6 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
